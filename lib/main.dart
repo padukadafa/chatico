3c77bd980f4fc5bf6953ce6bcfe895640f2e4799
padukadafa <padukadafa@gmail.com>
2024-07-29 00:11:13 +0700

    add: bottomnavbar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:chatico/common/services/notification_service.dart';
 import 'package:chatico/core/router/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 import 'firebase_options.dart';
 
@@ -14,7 +15,7 @@ void main() async {
   );
   NotificationService.initilizeNotifification();
   GetIt.instance.registerSingleton<AppRouter>(AppRouter());
-
+  await dotenv.load(fileName: '.env');
   runApp(MainApp());
 }
 
